b89f2ef53b875222dffaff9a75f7aacfb52d06e4
fpetrikovich <fpetrikovich@itba.edu.ar>
2020-10-03 14:10:25 -0300

    Beeman integrator skeleton established, missing future acceleration calculations

--- src/main/java/Integrator.java
+++ src/main/java/Integrator.java
@@ -11,7 +11,34 @@ public class Integrator {
         return exponential * cosine;
     }
 
-    public void beeman(Particle particle, double dt) {
-        
+    public void beeman(Particle p, double dt) {
+
+        /* Calculating future accelerations TODO -> algo con la fuerza */
+        double predictedAx = 0.0;
+        double predictedAy = 0.0;
+
+        /* Predicting positions */
+        double predictedX = this.beemanPositionPrediction(p.getX(), p.getVx(), p.getAx(), p.getPrevAx(), dt);
+        double predictedY = this.beemanPositionPrediction(p.getY(), p.getVy(), p.getAy(), p.getPrevAy(), dt);
+
+        /* Predicting velocities */
+        double predictedVx = this.beemanVelocityPrediction(p.getVx(), p.getAx(), p.getPrevAx(), predictedAx, dt);
+        double predictedVy = this.beemanVelocityPrediction(p.getVy(), p.getAy(), p.getPrevAy(), predictedAy, dt);
+
+        /* Set particles future predictions */
+        p.setFutureX(predictedX);
+        p.setFutureY(predictedY);
+        p.setFutureVx(predictedVx);
+        p.setFutureVy(predictedVy);
+        p.setFutureAx(predictedAx);
+        p.setFutureAy(predictedAy);
+    }
+
+    private double beemanPositionPrediction(double r, double v, double a, double aPrev, double dt) {
+        return r + v*dt + (2.0/3.0)*a*dt*dt + (1.0/6.0)*aPrev*dt*dt;
+    }
+
+    private double beemanVelocityPrediction(double v, double a, double aPrev, double aFuture, double dt) {
+        return v + ((1.0/3.0)*aFuture + (5.0/6.0)*a + (1.0/6.0)*aPrev) * dt;
     }
 }
