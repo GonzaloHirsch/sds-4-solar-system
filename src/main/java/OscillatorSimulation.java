764cbacce87a9a021c28d2ad0456a95a78cd887e
fpetrikovich <fpetrikovich@itba.edu.ar>
2020-10-03 16:34:47 -0300

    Simulation class added

---
+++ src/main/java/OscillatorSimulation.java
@@ -0,0 +1,11 @@
+public class OscillatorSimulation {
+
+    private double mass  = 70; // kg
+    private double k     = Math.pow(10, 4); // N/m
+    private double gamma = 100; // kg/s
+    private double tf    = 5; // s
+
+    OscillatorSimulation() { }
+
+    
+}
